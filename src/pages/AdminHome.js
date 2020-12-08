208ce0def8b19b9736af7b0a7555ec384d0f86ed
LRCong <1323794482@qq.com>
2020-12-09 01:03:56 +0800

    完成管理系统登陆页面布局以及登录逻辑、未登录时重定向逻辑

--- src/pages/AdminHome.js
+++ src/pages/AdminHome.js
@@ -1,14 +1,29 @@
 import React from 'react'
 
+import axios from 'axios'
+
 export default class AdminHome extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            userName: ''
+        };
+    }
+
+    componentDidMount() {
+        const storageName = sessionStorage.getItem('userName');
+        axios.get('/data/login.json')
+            .then((data) => {
+                if (data.data.data.userName === storageName)
+                    this.setState({ userName: data.data.data.userName })
+                else
+                    this.props.history.replace('/admin/login');
+            });
     }
 
     render() {
         return <div>
-            后台管理页面
+            {this.state.userName}后台管理页面
         </div>;
     }
 }
--- src/pages/AdminLogin.js
+++ src/pages/AdminLogin.js
@@ -1,14 +1,56 @@
 import React from 'react'
+import axios from 'axios'
+
+import './AdminLogin.css'
 
 export default class AdminLogin extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            userName: '',
+            pwd: '',
+            info: ''
+        };
     }
 
+    handlePwdChange = (e) => {
+        this.setState({
+            pwd: e.target.value
+        });
+    };
+
+    handleUserNameChange = (e) => {
+        this.setState({
+            userName: e.target.value
+        });
+    };
+
+    handleSubmit = (e) => {
+        axios.get('/data/login.json')
+            .then((data) => {
+                if (data.data.data.userName === this.state.userName && data.data.data.pwd === this.state.pwd) {
+                    sessionStorage.setItem('userName', this.state.userName);
+                    this.props.history.push('/admin');
+                }
+                else
+                    alert('密码错误或者没有这个账户');
+            });
+
+    };
+
     render() {
-        return <div>
-            后台管理登录页面
+        return <div className='login-container'>
+            <div className='side-img'>
+                <img src='/logo512.png' alt="React"></img>
+            </div>
+            <div className='side-login'>
+                <div className='login-form'>
+                    <h1>Login in</h1>
+                    <input type='text' placeholder='USERNAME' onChange={this.handleUserNameChange} />
+                    <input type='password' placeholder='PASSWD' onChange={this.handlePwdChange} />
+                    <input type='submit' onClick={this.handleSubmit} />
+                </div>
+            </div>
         </div>;
     }
 }
